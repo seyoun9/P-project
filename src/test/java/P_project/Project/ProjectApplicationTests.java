1451e98b09678b2d91a97f31ea8aaded48edfcec
seyoung <knocpig03@gmail.com>
2024-08-09 11:17:43 +0900

    테스트 파일 입니다.

---
+++ src/test/java/P_project/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package P_project.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
