9924df93b776fa1e9f2eb3c6864971f77735c558
Clue03 <knocpig03@gmail.com>
2024-09-22 12:40:59 +0900

    seyoung's branch

--- src/main/java/P_project/Project/controller/HomeController.java
+++ src/main/java/P_project/Project/controller/HomeController.java
@@ -1,2 +1,13 @@
-package P_project.Project.controller;public class HomeController {
+package P_project.Project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "";
+    }
 }
